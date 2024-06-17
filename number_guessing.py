f8456a1ef42d5e7f5cf67783012b1f6bf2219f97
maggiekuan <magdeline.kuan@gmail.com>
2024-06-17 23:54:26 +0900

    added another hint option - not done

--- number_guessing.py
+++ number_guessing.py
@@ -1,36 +1,57 @@
 import random
 
+goal = random.randint(100, 999)
+
+max_guesses = 3
+current_guesses = 0
+
+def compare_digits(guess):
+
+    goal_str = str(goal)
+    guess_str = str(guess)
+
+    for i in range(3):
+        if guess_str[i] < goal_str[i]:
+            print("↑")
+        elif guess_str[i] == goal_str[i]:
+            print("=")
+        else:
+            print("↓")
+
 def number_guessing():
 
-    goal = random.randint(100, 999)
-
-    max_guesses = 3
-    current_guesses = 0
+    global current_guesses
 
     while current_guesses < max_guesses:
 
-        if current_guesses == max_guesses-1:
-            print("You've run out of guesses :( the correct number was {goal})")
+        guess = int(input(f"Guess {current_guesses + 1}: "))
+
+        if guess < 100 or guess > 999:
+            print("Please enter a 3-digit number.")
+        
+        elif guess == goal:
+            print("Correct!")
             break
 
         else:
-            guess = int(input(f"Guess {current_guesses + 1}: "))
-
-            if guess < 100 or guess > 999:
-                print("Please enter a 3-digit number.")
-        
-            elif guess == goal:
-                print("Correct!")
-                break
+            if guess < goal:
+                print("The number is higher")
 
             else:
-                if guess < goal:
-                    print("The number is higher")
+                print("The number is lower")
 
-                else:
-                    print("The number is lower")
+            hint2 = input("Want another hint? (y/n) ")
+            if hint2 == "y":
+                compare_digits(guess)
+            elif hint2 == "n":
+                continue
+            else:
+                print("Please type y or n.")
 
-                current_guesses += 1
+            current_guesses += 1
+
+    if current_guesses == max_guesses:
+        print(f"You've run out of guesses :( The correct number was {goal}")
 
 def main():
     print("Guess a random 3-digit number!")
