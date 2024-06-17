02c2919c2080f7c03906ef8744f3ae251ceb37f7
maggiekuan <magdeline.kuan@gmail.com>
2024-06-17 23:08:03 +0900

    fixed while loop

--- number_guessing.py
+++ number_guessing.py
@@ -7,25 +7,30 @@ def number_guessing():
     max_guesses = 3
     current_guesses = 0
 
-    if current_guesses == max_guesses:
-        print("You've run out of guesses :( the correct number was {goal})")
+    while current_guesses < max_guesses:
 
-    else:
-        guess = int(input(f"Guess {current_guesses + 1}: "))
-
-        if guess < 100 or guess > 999:
-            print("Please enter a 3-digit number.")
-        
-        elif guess == goal:
-            print("Correct!")
+        if current_guesses == max_guesses-1:
+            print("You've run out of guesses :( the correct number was {goal})")
+            break
 
         else:
-            if guess < goal:
-                print("The number is higher")
+            guess = int(input(f"Guess {current_guesses + 1}: "))
+
+            if guess < 100 or guess > 999:
+                print("Please enter a 3-digit number.")
+        
+            elif guess == goal:
+                print("Correct!")
+                break
 
             else:
-                print("The number is lower")
-            current_guesses += 1
+                if guess < goal:
+                    print("The number is higher")
+
+                else:
+                    print("The number is lower")
+
+                current_guesses += 1
 
 def main():
     print("Guess a random 3-digit number!")
