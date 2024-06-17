974348ff645e0372998a908cd652e9a2240e7a0e
maggiekuan <magdeline.kuan@gmail.com>
2024-06-17 22:17:27 +0900

    created number_guessing function -> number_guessing.py

--- number_guessing.py
+++ number_guessing.py
@@ -8,3 +8,20 @@ def number_guessing():
     current_guesses = 0
 
     print("Guess a random 3-digit number!")
+
+    while max_guesses < current_guesses:
+        guess = int(input(f"Guess {current_guesses + 1}: "))
+
+        if guess < 100 or guess > 999:
+            print("Please enter a 3-digit number.")
+        
+        elif guess == goal:
+            print("Correct!")
+
+        else:
+            if guess < goal:
+                print("The number is higher")
+
+            else:
+                print("The number is lower")
+
