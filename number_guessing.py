e9fa676f2a25ad06c9d3e3df25b61d3baa796666
maggiekuan <magdeline.kuan@gmail.com>
2024-06-22 18:40:57 +0900

    fixed hint2 + while loop

--- number_guessing.py
+++ number_guessing.py
@@ -23,35 +23,38 @@ def number_guessing():
     global current_guesses
 
     while current_guesses < max_guesses:
+        try:
+            guess = int(input(f"Guess {current_guesses + 1}: "))
 
-        guess = int(input(f"Guess {current_guesses + 1}: "))
-
-        if guess < 100 or guess > 999:
-            print("Please enter a 3-digit number.")
+            if guess < 100 or guess > 999:
+                print("Please enter a 3-digit number.")
         
-        elif guess == goal:
-            print("Correct!")
-            break
-
-        else:
-            if guess < goal:
-                print("The number is higher")
+            elif guess == goal:
+                print("Correct!")
+                break
 
             else:
-                print("The number is lower")
+                current_guesses += 1
+                if current_guesses == max_guesses:
+                    print(f"You've run out of guesses :( The correct number was {goal}")
+                    break
+                if guess < goal:
+                    print("The number is higher.")
+                else:
+                    print("The number is lower.")
 
-            hint2 = input("Want another hint? (y/n) ")
-            if hint2 == "y":
-                compare_digits(guess)
-            elif hint2 == "n":
-                continue
-            else:
-                print("Please type y or n.")
+                hint2 = input("Want another hint? (y/n) ")
+                if hint2 == "y":
+                    compare_digits(guess)
+                elif hint2 == "n":
+                    pass
+                else:
+                    print("Please type y or n.")
 
-            current_guesses += 1
+        except ValueError:
+            print("Invalid input. Please enter a valid 3-digit number.")
 
-    if current_guesses == max_guesses:
-        print(f"You've run out of guesses :( The correct number was {goal}")
+        
 
 def main():
     print("Guess a random 3-digit number!")
