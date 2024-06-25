513cca9b439f115a4e3dce41b4fee6db3db90814
maggiekuan <magdeline.kuan@gmail.com>
2024-06-25 21:30:25 +0900

    added more Japanese Translation

--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -8,7 +8,9 @@ current_guesses = 0
 
 def main():
     print("Welcome to Number Guessing Game! If you are unsure of anything, please read the README.")
+    print("ようこそ数字当てゲームへ! 何か不明な点があれば、READMEを読んでください。")
     print("Let's begin: Guess a random 3-digit number!")
+    print("始めましょう: ランダムな三桁の数字を当ててみてください!")
     number_guessing.number_guessing(goal, max_guesses, current_guesses)
 
 if __name__ == "__main__":
