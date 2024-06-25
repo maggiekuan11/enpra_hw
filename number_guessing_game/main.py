96361dc067a7443906a662e2e30419217c26e6c4
maggiekuan <magdeline.kuan@gmail.com>
2024-06-25 21:55:49 +0900

    Added scoring feature + error handling for extra hints

--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -5,13 +5,15 @@ goal = random.randint(100, 999)
 
 max_guesses = 5
 current_guesses = 0
+max_points = 10
 
 def main():
     print("Welcome to Number Guessing Game! If you are unsure of anything, please read the README.")
     print("ようこそ数字当てゲームへ! 何か不明な点があれば、READMEを読んでください。")
+    print()
     print("Let's begin: Guess a random 3-digit number!")
     print("始めましょう: ランダムな三桁の数字を当ててみてください!")
-    number_guessing.number_guessing(goal, max_guesses, current_guesses)
+    number_guessing.number_guessing(goal, max_guesses, current_guesses, max_points)
 
 if __name__ == "__main__":
     main()
--- number_guessing_game/number_guessing.py
+++ number_guessing_game/number_guessing.py
@@ -1,6 +1,7 @@
 import hints
 
-def number_guessing(goal, max_guesses, current_guesses):
+def number_guessing(goal, max_guesses, current_guesses, max_points):
+    current_points = max_points
 
     while current_guesses < max_guesses:
         try:
@@ -11,32 +12,40 @@ def number_guessing(goal, max_guesses, current_guesses):
         
             elif guess == goal:
                 print("正解!")
+                print(f"Points = {current_points} / {max_points}")
                 break
 
             else:
                 current_guesses += 1
                 if current_guesses == max_guesses:
                     print(f"推測の回数が尽きました :( 正解の数字は {goal}でした。")
+                    print(f"Points = {current_points} / {max_points}")
                     break
 
                 hints.higher_lower(guess, goal)
 
                 hint2 = input("もう一つのヒントが欲しい? (y/n) ")
+                while hint2 != "y" and hint2 != "n":
+                    print("「y」か「n」を入力してください。")
+                    hint2 = input("もう一つのヒントが欲しい? (y/n) ")
                 if hint2 == "y":
+                    current_points -= 1
                     hints.compare_digits(guess, goal)
 
                     hint3 = input("第三のヒントが欲しい? (y/n) ")
-                    if hint3 == "y":
-                        hints.even_odd(goal)
-                    elif hint3 == "n":
-                        pass
-                    else:
+                    while hint3 != "y" and hint3 != "n":
                         print("「y」か「n」を入力してください。")
+                        hint3 = input("第三のヒントが欲しい? (y/n) ")
+
+                    if hint3 == "y":
+                        current_points -= 1
+                        hints.even_odd(goal)
+                    else:
+                        pass
                         
-                elif hint2 == "n":
-                    pass
                 else:
-                    print("「y」か「n」を入力してください。")
+                    pass
+                
 
         except ValueError:
             print("Invalid input. 三桁の数字を入力してください。")
